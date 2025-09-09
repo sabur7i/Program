b59aab1a92d9493edb4dcf3f5357fa587261a4d1
Ettore Saburi Baldo <ettore.baldo@sou.unaerp.edu.br>
2025-09-09 12:43:09 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
 
-public class Program {
+public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
